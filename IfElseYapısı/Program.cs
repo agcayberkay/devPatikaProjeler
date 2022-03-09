e5d25a29546a068fa5867695c73b55cd1c415a81
Berkay Ağçay <95404939+agcayberkay@users.noreply.github.com>
2022-03-09 17:34:42 +0300

    Add files via upload

---
+++ IfElseYapısı/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IfElseYapısı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool dogrumu = true;
+
+            if (dogrumu==true)
+            {
+                dogrumu=false;
+                System.Console.WriteLine("false");
+            }
+            else
+            {
+                dogrumu=true;
+                System.Console.WriteLine("true");
+            }
+        }
+    }
+}
