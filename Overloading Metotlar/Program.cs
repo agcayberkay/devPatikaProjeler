a159808f7855eb643e4edca1d8303cd5ca18460e
Berkay Ağçay <95404939+agcayberkay@users.noreply.github.com>
2022-03-09 19:14:55 +0300

    Add files via upload

---
+++ Overloading Metotlar/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Overloading_Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sayi="122";
+
+            bool b1 = int.TryParse(sayi, out int sayi2);
+            if (b1)
+            {
+                System.Console.WriteLine("Başarılı");
+                    System.Console.WriteLine("Out Sayı: "+sayi2);
+            }
+            else
+            {
+                System.Console.WriteLine("Başarısız");
+            }
+
+            Methot m1 = new Methot();
+            m1.EkranaYazdır(1);
+            m1.EkranaYazdır(2,"3");
+            m1.EkranaYazdır("2");
+        }
+    }
+
+    class Methot
+    {
+        //OVERLOADİNG
+        public void EkranaYazdır(int sayi1)
+        {
+            System.Console.WriteLine(sayi1);
+        }
+
+         public void EkranaYazdır(string sayi1)
+        {
+            System.Console.WriteLine(sayi1);
+        }
+
+         public void EkranaYazdır(int sayi1,string sayi2)
+        {
+            System.Console.WriteLine(sayi1 + sayi2);
+        }
+
+
+
+        
+    }
+}
