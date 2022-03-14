c41faf83ceb69bd3dc6c000b16ef52bc720a5e27
Berkay Ağçay <95404939+agcayberkay@users.noreply.github.com>
2022-03-14 12:25:14 +0300

    Add files via upload

---
+++ Enum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Lütfen bir sıcaklık giriniz.");
+            int sıcaklık =Convert.ToInt32(Console.ReadLine());
+           if(sıcaklık<= (int)HavaDurumu.Normal)        
+               System.Console.WriteLine("We shouldn't go out.");
+               else if (sıcaklık >= (int)HavaDurumu.Sıcak)
+                   System.Console.WriteLine("It's too hot to go outside");
+                else if (sıcaklık >= (int)HavaDurumu.Normal && sıcaklık <= (int)HavaDurumu.CokSıcak)
+                    System.Console.WriteLine("Lets go out");
+
+           System.Console.WriteLine(Gunler.Cuma);
+           System.Console.WriteLine((int)Gunler.Cumartesi);
+        }
+    }
+
+    enum Gunler 
+    {
+        Pazartesi=1, Salı,Çarşamba,Perşembe,Cuma,Cumartesi,Pazar
+    }
+
+
+    enum HavaDurumu 
+    {
+        CokSoguk =-5,
+        Soguk = 5,
+        Sıcak=30,
+        Normal = 20,
+        CokSıcak=40
+    }
+}
